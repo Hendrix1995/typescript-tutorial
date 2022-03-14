ddab00b6ee3705cc0f6ee931ff198429df59c837
Hendrix1995 <hgud55@naver.com>
2022-03-14 18:18:22 +0900

    fix: DateFilter can get date search term

--- src/components/chart/DateFilter.tsx
+++ src/components/chart/DateFilter.tsx
@@ -1,15 +1,6 @@
 import { useState, useEffect } from "react";
 import styled from "styled-components";
-
-type DateType = {
-    getDateStr: any;
-    startDateHandler: any;
-    endDateHandler: any;
-    startDate: string;
-    endDate: string;
-    today: any;
-    day: any;
-};
+import axios from "axios";
 
 const DateFilterContainer = styled.div`
     display: flex;
@@ -29,7 +20,7 @@ const DateBox = styled.div`
     margin: 1px;
 `;
 
-const Date = styled.input`
+const DateInput = styled.input`
     position: relative;
     font-size: 16px;
 `;
@@ -52,46 +43,76 @@ const FilterButton = styled.button`
     }
 `;
 
-type DayListType = {
-    lastWeek: string;
-    lastMonth: string;
-    sixMonth: string;
-    oneYear: string;
-};
+function DateFilter() {
+    const getDateStr = (myDate: any) => {
+        let month = myDate.getMonth();
+        let toStringMonth = "";
+        if (month < 10) toStringMonth = "0" + String(month + 1);
+        else toStringMonth = String(month + 1);
+        let day = myDate.getDate();
+        let toStringDay = "";
+        if (String(day).length === 1) toStringDay = "0" + String(day);
+        else toStringDay = String(day);
+        return `${myDate.getFullYear()}-${toStringMonth}-${toStringDay}`;
+    };
 
-function DateFilter({ day, getDateStr, startDateHandler, endDateHandler, startDate, endDate, today }: DateType) {
+    const today = () => {
+        const day = new Date();
+        return getDateStr(day);
+    };
+
     const lastWeek = () => {
+        const day = new Date();
         const dayOfMonth = day.getDate();
         day.setDate(dayOfMonth - 7);
         return getDateStr(day);
     };
 
     const lastMonth = () => {
+        const day = new Date();
         const monthOfYear = day.getMonth();
         day.setMonth(monthOfYear - 1);
         return getDateStr(day);
     };
 
     const sixMonth = () => {
+        const day = new Date();
         const monthOfYear = day.getMonth();
-        day.setMonth(monthOfYear - 5);
+        day.setMonth(monthOfYear - 6);
         return getDateStr(day);
     };
 
     const oneYear = () => {
+        const day = new Date();
         const dayOfYear = day.getFullYear();
-        day.setDate(dayOfYear - 1);
+        day.setFullYear(dayOfYear - 1);
         return getDateStr(day);
     };
 
-    const [dayList, setDayList] = useState<DayListType>({ lastWeek: lastWeek(), lastMonth: lastMonth(), sixMonth: sixMonth(), oneYear: oneYear() });
-    console.log(dayList);
+    const [startDate, setStartDate] = useState<string>(today());
+    const [endDate, setEndDate] = useState<string>(today());
+
+    const startDateHandler = (date: string) => {
+        setStartDate(date);
+    };
+    const endDateHandler = (date: string) => {
+        setEndDate(date);
+    };
+
+    const getDateHandler = async () => {
+        const { data } = await axios.get(`http://dev-admin.ittang.co.kr/api/statistic/user?startDate=${startDate}&endDate=${endDate}`, {
+            headers: {
+                Authorization: `bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsib2FzaXNidXNpbmVzcyJdLCJhZG1pbl91c2VyX2lkIjozNCwidXNlcl9uYW1lIjoiSUQ6aHMubGltIiwic2NvcGUiOlsicmVhZCJdLCJleHAiOjE2NDczMDU3MTUsImF1dGhvcml0aWVzIjpbIlJPTEVfUEFZTUVOVF9SRUFEIiwiUk9MRV9BUlRJQ0xFIiwiUk9MRV9JVEVNIiwiUk9MRV9QQVlNRU5UX1VQREFURSIsIlJPTEVfUEFZTUVOVCIsIlJPTEVfUEFSVE5FUiIsIlJPTEVfSVRMT1VOREdFIiwiUk9MRV9EQVNIQk9BUkQiLCJST0xFX1VTRVIiLCJST0xFX05PVElDRSIsIlJPTEVfR09PRFMiLCJST0xFX0FETUlOIiwiUk9MRV9DT1VQT04iXSwianRpIjoiODVkYWM0MzAtY2I3Yi00YzE0LWI2OGQtN2EzZmU4MzdmZjlhIiwiY2xpZW50X2lkIjoib2FzaXNidXNpbmVzcyJ9.GPb0_wd_UvsBYVut1RtGsaLUSXCq9IsrG3FiDH0TrgU`,
+            },
+        });
+        return data;
+    };
 
     return (
         <DateFilterContainer>
             <DateBox>
-                <Date type="date" value={startDate} onChange={(e) => startDateHandler(e.target.value)} />
-                <Date type="date" value={endDate} onChange={(e) => endDateHandler(e.target.value)} />
+                <DateInput type="date" value={startDate} onChange={(e) => startDateHandler(e.target.value)} />
+                <DateInput type="date" value={endDate} onChange={(e) => endDateHandler(e.target.value)} />
             </DateBox>
             <FilterButtonBox>
                 <FilterButton
--- src/page/chart.tsx
+++ src/page/chart.tsx
@@ -62,41 +62,6 @@ const replaceNumHandler = (num: number) => {
 function chart() {
     const { isLoading, data, isError } = useQuery("total", getTotalHandler);
 
-    const getDateStr = (myDate: any) => {
-        let month = myDate.getMonth();
-        let toStringMonth = "";
-        if (month < 10) toStringMonth = "0" + String(month + 1);
-        else toStringMonth = String(month + 1);
-        let day = myDate.getDate();
-        let toStringDay = "";
-        if (String(day).length === 1) toStringDay = "0" + String(day);
-        else toStringDay = String(day);
-        return `${myDate.getFullYear()}-${toStringMonth}-${toStringDay}`;
-    };
-
-    const day = new Date();
-    const today = () => {
-        return getDateStr(day);
-    };
-
-    const [startDate, setStartDate] = useState<string>(today());
-    const [endDate, setEndDate] = useState<string>(today());
-    const startDateHandler = (date: string) => {
-        setStartDate(date);
-    };
-    const endDateHandler = (date: string) => {
-        setEndDate(date);
-    };
-
-    const getDateHandler = async () => {
-        const { data } = await axios.get(`http://dev-admin.ittang.co.kr/api/statistic/user?startDate=${startDate}&endDate=${endDate}`, {
-            headers: {
-                Authorization: `bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsib2FzaXNidXNpbmVzcyJdLCJhZG1pbl91c2VyX2lkIjozNCwidXNlcl9uYW1lIjoiSUQ6aHMubGltIiwic2NvcGUiOlsicmVhZCJdLCJleHAiOjE2NDczMDU3MTUsImF1dGhvcml0aWVzIjpbIlJPTEVfUEFZTUVOVF9SRUFEIiwiUk9MRV9BUlRJQ0xFIiwiUk9MRV9JVEVNIiwiUk9MRV9QQVlNRU5UX1VQREFURSIsIlJPTEVfUEFZTUVOVCIsIlJPTEVfUEFSVE5FUiIsIlJPTEVfSVRMT1VOREdFIiwiUk9MRV9EQVNIQk9BUkQiLCJST0xFX1VTRVIiLCJST0xFX05PVElDRSIsIlJPTEVfR09PRFMiLCJST0xFX0FETUlOIiwiUk9MRV9DT1VQT04iXSwianRpIjoiODVkYWM0MzAtY2I3Yi00YzE0LWI2OGQtN2EzZmU4MzdmZjlhIiwiY2xpZW50X2lkIjoib2FzaXNidXNpbmVzcyJ9.GPb0_wd_UvsBYVut1RtGsaLUSXCq9IsrG3FiDH0TrgU`,
-            },
-        });
-        return data;
-    };
-
     return (
         <>
             <PageButton>Go to Home</PageButton>
@@ -128,7 +93,7 @@ function chart() {
                             </NumInfoBox>
                         </NumInfoContainer>
                         <hr style={{ margin: "32px 0", borderColor: "rgba(0,0,21,.2)" }} />
-                        <DateFilter day={day} getDateStr={getDateStr} startDateHandler={startDateHandler} endDateHandler={endDateHandler} startDate={startDate} endDate={endDate} today={today} />
+                        <DateFilter />
                         <Charts />
                     </MainBody>
                 </CardBody>
