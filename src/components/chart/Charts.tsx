d9c15293191881ed426895eba3d1ed29788bb610
Hendrix1995 <hgud55@naver.com>
2022-03-16 13:20:17 +0900

    feat: chart complete

--- src/components/chart/Charts.tsx
+++ src/components/chart/Charts.tsx
@@ -1,14 +1,24 @@
 import styled from "styled-components";
-import { LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line, PieChart, Pie, Cell, Label } from "recharts";
+import { LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line, PieChart, Pie, Cell, ResponsiveContainer, BarChart, Bar } from "recharts";
 import { useQuery } from "react-query";
 import { format, addDays, differenceInDays } from "date-fns";
 import axios from "axios";
+
+const PieChartContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const BarChartContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 function Charts({ startDate, endDate }: { startDate: string; endDate: string }) {
   const getDateHandler = async () => {
     const { data } = await axios.get(`http://dev-admin.ittang.co.kr/api/statistic/user?startDate=${startDate}&endDate=${endDate}`, {
       headers: {
-        Authorization: `bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsib2FzaXNidXNpbmVzcyJdLCJhZG1pbl91c2VyX2lkIjozNCwidXNlcl9uYW1lIjoiSUQ6aHMubGltIiwic2NvcGUiOlsicmVhZCJdLCJleHAiOjE2NDczOTIyMTUsImF1dGhvcml0aWVzIjpbIlJPTEVfUEFZTUVOVF9SRUFEIiwiUk9MRV9BUlRJQ0xFIiwiUk9MRV9JVEVNIiwiUk9MRV9QQVlNRU5UX1VQREFURSIsIlJPTEVfUEFZTUVOVCIsIlJPTEVfUEFSVE5FUiIsIlJPTEVfSVRMT1VOREdFIiwiUk9MRV9EQVNIQk9BUkQiLCJST0xFX1VTRVIiLCJST0xFX05PVElDRSIsIlJPTEVfR09PRFMiLCJST0xFX0FETUlOIiwiUk9MRV9DT1VQT04iXSwianRpIjoiM2M1M2U0ZDctM2MyMi00YWNmLThjNjktYTY3ZDYxYjlkYWZiIiwiY2xpZW50X2lkIjoib2FzaXNidXNpbmVzcyJ9.A27WyercfiYA5fynusQBTZA5L44schq7-LgkLRWRxio`,
+        Authorization: `bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsib2FzaXNidXNpbmVzcyJdLCJhZG1pbl91c2VyX2lkIjozNCwidXNlcl9uYW1lIjoiSUQ6aHMubGltIiwic2NvcGUiOlsicmVhZCJdLCJleHAiOjE2NDc0Nzg2NDYsImF1dGhvcml0aWVzIjpbIlJPTEVfUEFZTUVOVF9SRUFEIiwiUk9MRV9BUlRJQ0xFIiwiUk9MRV9JVEVNIiwiUk9MRV9QQVlNRU5UX1VQREFURSIsIlJPTEVfUEFZTUVOVCIsIlJPTEVfUEFSVE5FUiIsIlJPTEVfSVRMT1VOREdFIiwiUk9MRV9EQVNIQk9BUkQiLCJST0xFX1VTRVIiLCJST0xFX05PVElDRSIsIlJPTEVfR09PRFMiLCJST0xFX0FETUlOIiwiUk9MRV9DT1VQT04iXSwianRpIjoiMWFjYWUxZjAtNzU3Yi00ZGE0LThjZmEtMzE5YjIyYzRlYWIxIiwiY2xpZW50X2lkIjoib2FzaXNidXNpbmVzcyJ9.53GR1quxYXjfE23mduEd40B3tYWmO8Yy0wJMkpX4weo`,
       },
     });
     if (data !== undefined) {
@@ -44,56 +54,195 @@ function Charts({ startDate, endDate }: { startDate: string; endDate: string }) 
     }
   };
 
-  const ChartContainer = styled.div``;
-
-  const PieChartContainer = styled.div``;
-
-  const PieChartBox = styled.div``;
-
   if (isError) {
     return <div>에러!!!</div>;
   }
 
-  console.log(data);
-  const pieColors = ["#fbb4ae", "#b3cde3"];
+  const changeKey = (arr: any[]) => {
+    let result: any = [];
+    arr.forEach((obj) => {
+      if (obj.hasOwnProperty("objKey")) {
+        obj.name = obj.objKey;
+        delete obj.objKey;
+      }
+      result.push(obj);
+    });
+    return result;
+  };
+
+  const ageChangeValue = (arr: any[]) => {
+    let result: any = [];
+    arr.forEach((el: any, index: number) => {
+      let temp = el.objKey;
+      if (temp === "UNKNOWN") {
+        result.push({ objKey: "알수없음", count: el.count });
+      } else {
+        result.push({ objKey: temp + "대", count: el.count });
+      }
+    });
+    return result;
+  };
+
+  const genderColorPicker = (el: any) => {
+    if (el.name === "FEMALE") return "#fbb4ae";
+    if (el.name === "MALE") return "#b3cde3";
+    else return "#CCEBC5";
+  };
+
+  const SocialColorPicker = (el: any) => {
+    if (el.name === "KAKAO") return "#FDD100";
+    if (el.name === "NAVER") return "#19CE60";
+    else return "#333333";
+  };
 
   return (
     <>
       {isLoading ? (
-        <div>Loading...</div>
+        <div style={{ display: "flex", justifyContent: "center", margin: "30px" }}>Loading...</div>
       ) : (
-        <ChartContainer>
-          <>
-            <h4 style={{ margin: "20px" }}>일자별 가입 회원수</h4>
-            {data.countByDate.length === 0 ? (
-              <div>불러올 데이터가 없습니다</div>
-            ) : (
-              <ChartContainer>
-                <LineChart width={1030} height={250} data={addNullDate(data?.countByDate)}>
-                  <CartesianGrid strokeDasharray="5 5" />
-                  <XAxis dataKey="date" />
-                  <YAxis dataKey="count" allowDecimals={false} allowDataOverflow={true} scale="linear" />
-                  <Tooltip />
-                  <Legend />
-                  <Line type="monotone" dataKey="count" stroke="#f9b115" />
-                </LineChart>
-              </ChartContainer>
-            )}
-          </>
-          <PieChartContainer>
-            <PieChartBox>
-              <h4 style={{ margin: "20px" }}>성별</h4>
-              <PieChart width={330} height={300}>
-                <Pie data={data?.countGenderByDate} dataKey="count" cx="50%" cy="50%" outerRadius={120} fill="#8884d8">
-                  {data?.countGenderByDate.map((el: any, index: number) => (
-                    <Cell key={`cell-${index}`} fill={pieColors[index]} />
-                  ))}
-                </Pie>
-                <Tooltip />
-              </PieChart>
-            </PieChartBox>
-          </PieChartContainer>
-        </ChartContainer>
+        <>
+          {data.countByDate.length === 0 ? (
+            <div style={{ display: "flex", justifyContent: "center", margin: "30px" }}>불러올 데이터가 없습니다</div>
+          ) : (
+            <>
+              <h4 style={{ margin: "20px" }}>일자별 가입 회원수</h4>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <ResponsiveContainer width="80%" height={280}>
+                  <LineChart data={addNullDate(data?.countByDate)}>
+                    <CartesianGrid strokeDasharray="5 5" />
+                    <XAxis dataKey="date" />
+                    <YAxis dataKey="count" allowDecimals={false} allowDataOverflow={true} scale="linear" />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="count" stroke="#f9b115" />
+                  </LineChart>
+                </ResponsiveContainer>
+              </div>
+              <PieChartContainer>
+                <div>
+                  <h4 style={{ margin: "20px" }}>성별</h4>
+                  <PieChart width={420} height={380}>
+                    <Pie
+                      data={changeKey(data?.countGenderByDate)}
+                      dataKey="count"
+                      cx="50%"
+                      cy="50%"
+                      outerRadius={120}
+                      label={({ cx, cy, midAngle, innerRadius, outerRadius, index }) => {
+                        const RADIAN = Math.PI / 180;
+                        const radius = 25 + innerRadius + (outerRadius - innerRadius) + 10;
+                        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+                        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+                        const inRadius = innerRadius + (outerRadius - innerRadius) * 0.5;
+                        const inX = cx + inRadius * Math.cos(-midAngle * RADIAN);
+                        const inY = cy + inRadius * Math.sin(-midAngle * RADIAN);
+                        return (
+                          <>
+                            <text x={inX} y={inY} textAnchor={inX > cx ? "start" : "end"} dominantBaseline="central">
+                              {data?.countGenderByDate[index].count}
+                            </text>
+                            <text x={x} y={y} textAnchor={x > cx ? "start" : "end"} dominantBaseline="central">
+                              {data?.countGenderByDate[index].name === "FEMALE" ? "여성" : null}
+                              {data?.countGenderByDate[index].name === "MALE" ? "남성" : null}
+                              {data?.countGenderByDate[index].name === "UNKNOWN" ? "알수없음" : null}
+                            </text>
+                          </>
+                        );
+                      }}
+                    >
+                      {data?.countGenderByDate.map((el: any, index: number) => (
+                        <Cell key={`cell-${index}`} fill={genderColorPicker(el)} />
+                      ))}
+                    </Pie>
+                    <Tooltip />
+                    <Legend
+                      content={() => (
+                        <div style={{ display: "flex", justifyContent: "space-around" }}>
+                          {data?.countGenderByDate.map((el: any, index: number) => (
+                            <div key={index} style={{ display: "flex", alignItems: "center" }}>
+                              <div style={{ width: "15px", height: "15px", backgroundColor: `${genderColorPicker(el)}`, borderRadius: "50px", marginRight: "5px" }} />
+                              <div>
+                                {el.name === "FEMALE" ? "여성" : null}
+                                {el.name === "MALE" ? "남성" : null}
+                                {el.name === "UNKNOWN" ? "알수없음" : null}
+                              </div>
+                            </div>
+                          ))}
+                        </div>
+                      )}
+                    />
+                  </PieChart>
+                </div>
+                <div>
+                  <h4 style={{ margin: "20px" }}>채널별</h4>
+                  <PieChart width={430} height={365}>
+                    <Pie
+                      data={changeKey(data?.countSocialTypeByDate)}
+                      dataKey="count"
+                      cx="50%"
+                      cy="50%"
+                      outerRadius={120}
+                      label={({ cx, cy, midAngle, innerRadius, outerRadius, index }) => {
+                        const RADIAN = Math.PI / 180;
+                        const radius = 25 + innerRadius + (outerRadius - innerRadius) + 10;
+                        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+                        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+                        const inRadius = innerRadius + (outerRadius - innerRadius) * 0.5;
+                        const inX = cx + inRadius * Math.cos(-midAngle * RADIAN);
+                        const inY = cy + inRadius * Math.sin(-midAngle * RADIAN);
+                        return (
+                          <>
+                            <text x={inX} y={inY} textAnchor={inX > cx ? "start" : "end"} dominantBaseline="central">
+                              {data?.countSocialTypeByDate[index].count}
+                            </text>
+                            <text x={x} y={y} textAnchor={x > cx ? "start" : "end"} dominantBaseline="central">
+                              {data?.countSocialTypeByDate[index].name === "KAKAO" ? "카카오" : null}
+                              {data?.countSocialTypeByDate[index].name === "NAVER" ? "네이버" : null}
+                              {data?.countSocialTypeByDate[index].name === "APPLE" ? "애플" : null}
+                            </text>
+                          </>
+                        );
+                      }}
+                    >
+                      {data?.countSocialTypeByDate.map((el: any, index: number) => (
+                        <Cell key={`cell-${index}`} fill={SocialColorPicker(el)} />
+                      ))}
+                    </Pie>
+                    <Tooltip />
+                    <Legend
+                      content={() => (
+                        <div style={{ display: "flex", justifyContent: "space-around" }}>
+                          {data?.countSocialTypeByDate.map((el: any, index: number) => (
+                            <div key={index} style={{ display: "flex", alignItems: "center" }}>
+                              <div style={{ width: "15px", height: "15px", backgroundColor: `${SocialColorPicker(el)}`, borderRadius: "50px", marginRight: "5px" }} />
+                              <div>
+                                {el.name === "KAKAO" ? "카카오" : null}
+                                {el.name === "NAVER" ? "네이버" : null}
+                                {el.name === "APPLE" ? "애플" : null}
+                              </div>
+                            </div>
+                          ))}
+                        </div>
+                      )}
+                    />
+                  </PieChart>
+                </div>
+              </PieChartContainer>
+              <h4 style={{ margin: "40px" }}>연령별</h4>
+              <BarChartContainer>
+                <ResponsiveContainer width="60%" height={200}>
+                  <BarChart width={150} height={40} data={ageChangeValue(data?.countAgeGroupByDate)} barSize={30}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="objKey" />
+                    <YAxis dataKey="count" allowDecimals={false} allowDataOverflow={true} />
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <Bar dataKey="count" fill="#a6cee3" background={{ fill: "#eee" }} label={{ position: "top" }} />
+                  </BarChart>
+                </ResponsiveContainer>
+              </BarChartContainer>
+            </>
+          )}
+        </>
       )}
     </>
   );
--- src/page/chart.tsx
+++ src/page/chart.tsx
@@ -10,140 +10,132 @@ import DateFilter from "../components/chart/DateFilter";
 import Charts from "../components/chart/Charts";
 
 const CardBody = styled.div`
-    background-color: #ebedef;
-    padding: 20px 14px;
+  background-color: #ebedef;
+  padding: 20px 14px;
 `;
 
 const MainBody = styled.div`
-    background-color: #fff;
-    padding: 20px 14px;
-    border: 1px solid #d8dbe0;
-    border-radius: 4px;
+  background-color: #fff;
+  padding: 20px 14px;
+  border: 1px solid #d8dbe0;
+  border-radius: 4px;
 `;
 
 const NumInfoContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 36px;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 36px;
 `;
 
 const NumInfoBox = styled.div`
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    justify-content: center;
-    font-size: 20px;
-    border: 1px solid #d8dbe0;
-    border-radius: 4px;
-    height: 100px;
-    width: 210px;
-    padding: 16px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  font-size: 20px;
+  border: 1px solid #d8dbe0;
+  border-radius: 4px;
+  height: 100px;
+  width: 210px;
+  padding: 16px;
 `;
 
 const NumInfo = styled.div`
-    display: block;
-    margin-left: auto;
-    font-size: 25px;
-    color: #3c4b64;
+  display: block;
+  margin-left: auto;
+  font-size: 25px;
+  color: #3c4b64;
 `;
 
 const getTotalHandler = async () => {
-    const { data } = await axios.get(`http://dev-admin.ittang.co.kr/api/statistic/user/total`, {
-        headers: {
-            Authorization: `bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsib2FzaXNidXNpbmVzcyJdLCJhZG1pbl91c2VyX2lkIjozNCwidXNlcl9uYW1lIjoiSUQ6aHMubGltIiwic2NvcGUiOlsicmVhZCJdLCJleHAiOjE2NDczOTIyMTUsImF1dGhvcml0aWVzIjpbIlJPTEVfUEFZTUVOVF9SRUFEIiwiUk9MRV9BUlRJQ0xFIiwiUk9MRV9JVEVNIiwiUk9MRV9QQVlNRU5UX1VQREFURSIsIlJPTEVfUEFZTUVOVCIsIlJPTEVfUEFSVE5FUiIsIlJPTEVfSVRMT1VOREdFIiwiUk9MRV9EQVNIQk9BUkQiLCJST0xFX1VTRVIiLCJST0xFX05PVElDRSIsIlJPTEVfR09PRFMiLCJST0xFX0FETUlOIiwiUk9MRV9DT1VQT04iXSwianRpIjoiM2M1M2U0ZDctM2MyMi00YWNmLThjNjktYTY3ZDYxYjlkYWZiIiwiY2xpZW50X2lkIjoib2FzaXNidXNpbmVzcyJ9.A27WyercfiYA5fynusQBTZA5L44schq7-LgkLRWRxio`,
-        },
-    });
-    return data;
+  const { data } = await axios.get(`http://dev-admin.ittang.co.kr/api/statistic/user/total`, {
+    headers: {
+      Authorization: `bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsib2FzaXNidXNpbmVzcyJdLCJhZG1pbl91c2VyX2lkIjozNCwidXNlcl9uYW1lIjoiSUQ6aHMubGltIiwic2NvcGUiOlsicmVhZCJdLCJleHAiOjE2NDc0Nzg2NDYsImF1dGhvcml0aWVzIjpbIlJPTEVfUEFZTUVOVF9SRUFEIiwiUk9MRV9BUlRJQ0xFIiwiUk9MRV9JVEVNIiwiUk9MRV9QQVlNRU5UX1VQREFURSIsIlJPTEVfUEFZTUVOVCIsIlJPTEVfUEFSVE5FUiIsIlJPTEVfSVRMT1VOREdFIiwiUk9MRV9EQVNIQk9BUkQiLCJST0xFX1VTRVIiLCJST0xFX05PVElDRSIsIlJPTEVfR09PRFMiLCJST0xFX0FETUlOIiwiUk9MRV9DT1VQT04iXSwianRpIjoiMWFjYWUxZjAtNzU3Yi00ZGE0LThjZmEtMzE5YjIyYzRlYWIxIiwiY2xpZW50X2lkIjoib2FzaXNidXNpbmVzcyJ9.53GR1quxYXjfE23mduEd40B3tYWmO8Yy0wJMkpX4weo`,
+    },
+  });
+  return data;
 };
 
 const replaceNumHandler = (num: number) => {
-    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
 
 function chart() {
-    const navigate = useNavigate();
-    const { isLoading, data, isError } = useQuery("total", getTotalHandler);
+  const navigate = useNavigate();
+  const { isLoading, data, isError } = useQuery("total", getTotalHandler);
 
-    const getDateStr = (myDate: any) => {
-        let month = myDate.getMonth();
-        let toStringMonth = "";
-        if (month < 10) toStringMonth = "0" + String(month + 1);
-        else toStringMonth = String(month + 1);
-        let day = myDate.getDate();
-        let toStringDay = "";
-        if (String(day).length === 1) toStringDay = "0" + String(day);
-        else toStringDay = String(day);
-        return `${myDate.getFullYear()}-${toStringMonth}-${toStringDay}`;
-    };
+  const getDateStr = (myDate: any) => {
+    let month = myDate.getMonth();
+    let toStringMonth = "";
+    if (month < 10) toStringMonth = "0" + String(month + 1);
+    else toStringMonth = String(month + 1);
+    let day = myDate.getDate();
+    let toStringDay = "";
+    if (String(day).length === 1) toStringDay = "0" + String(day);
+    else toStringDay = String(day);
+    return `${myDate.getFullYear()}-${toStringMonth}-${toStringDay}`;
+  };
 
-    const today = () => {
-        const day = new Date();
-        return getDateStr(day);
-    };
+  const today = () => {
+    const day = new Date();
+    return getDateStr(day);
+  };
 
-    const lastMonth = () => {
-        const day = new Date();
-        const monthOfYear = day.getMonth();
-        day.setMonth(monthOfYear - 1);
-        return getDateStr(day);
-    };
+  const lastMonth = () => {
+    const day = new Date();
+    const monthOfYear = day.getMonth();
+    day.setMonth(monthOfYear - 1);
+    return getDateStr(day);
+  };
 
-    const [startDate, setStartDate] = useState<string>(lastMonth());
-    const [endDate, setEndDate] = useState<string>(today());
+  const [startDate, setStartDate] = useState<string>(lastMonth());
+  const [endDate, setEndDate] = useState<string>(today());
 
-    const startDateHandler = (date: string) => {
-        setStartDate(date);
-    };
-    const endDateHandler = (date: string) => {
-        setEndDate(date);
-    };
+  const startDateHandler = (date: string) => {
+    setStartDate(date);
+  };
+  const endDateHandler = (date: string) => {
+    setEndDate(date);
+  };
 
-    return (
-        <>
-            <PageButton onClick={() => navigate("/")}>Go to Home</PageButton>
-            {isLoading ? (
-                <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>Loading...</div>
-            ) : (
-                <CardBody>
-                    <MainBody>
-                        <NumInfoContainer>
-                            <NumInfoBox>
-                                전체 회원수
-                                <NumInfo>{replaceNumHandler(data.totalCount)}명</NumInfo>
-                            </NumInfoBox>
-                            <NumInfoBox>
-                                오늘 가입 회원수
-                                <NumInfo>{replaceNumHandler(data.todayCount)}명</NumInfo>
-                            </NumInfoBox>
-                            <NumInfoBox>
-                                활성 회원수
-                                <NumInfo>{replaceNumHandler(data.statusCount[1].count)}명</NumInfo>
-                            </NumInfoBox>
-                            <NumInfoBox>
-                                휴먼 회원수
-                                <NumInfo>0명</NumInfo>
-                            </NumInfoBox>
-                            <NumInfoBox>
-                                탈퇴 회원수
-                                <NumInfo>{replaceNumHandler(data.statusCount[0].count)}명</NumInfo>
-                            </NumInfoBox>
-                        </NumInfoContainer>
-                        <hr style={{ margin: "32px 0", borderColor: "rgba(0,0,21,.2)" }} />
-                        <DateFilter
-                            startDate={startDate}
-                            endDate={endDate}
-                            getDateStr={getDateStr}
-                            today={today}
-                            lastMonth={lastMonth}
-                            startDateHandler={startDateHandler}
-                            endDateHandler={endDateHandler}
-                        />
-                        <Charts startDate={startDate} endDate={endDate} />
-                    </MainBody>
-                </CardBody>
-            )}
-        </>
-    );
+  return (
+    <>
+      <PageButton onClick={() => navigate("/")}>Go to Home</PageButton>
+      {isLoading ? (
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>Loading...</div>
+      ) : (
+        <CardBody>
+          <MainBody>
+            <NumInfoContainer>
+              <NumInfoBox>
+                전체 회원수
+                <NumInfo>{replaceNumHandler(data.totalCount)}명</NumInfo>
+              </NumInfoBox>
+              <NumInfoBox>
+                오늘 가입 회원수
+                <NumInfo>{replaceNumHandler(data.todayCount)}명</NumInfo>
+              </NumInfoBox>
+              <NumInfoBox>
+                활성 회원수
+                <NumInfo>{replaceNumHandler(data.statusCount[1].count)}명</NumInfo>
+              </NumInfoBox>
+              <NumInfoBox>
+                휴먼 회원수
+                <NumInfo>0명</NumInfo>
+              </NumInfoBox>
+              <NumInfoBox>
+                탈퇴 회원수
+                <NumInfo>{replaceNumHandler(data.statusCount[0].count)}명</NumInfo>
+              </NumInfoBox>
+            </NumInfoContainer>
+            <hr style={{ margin: "32px 0", borderColor: "rgba(0,0,21,.2)" }} />
+            <DateFilter startDate={startDate} endDate={endDate} getDateStr={getDateStr} today={today} lastMonth={lastMonth} startDateHandler={startDateHandler} endDateHandler={endDateHandler} />
+            <Charts startDate={startDate} endDate={endDate} />
+          </MainBody>
+        </CardBody>
+      )}
+    </>
+  );
 }
 
 export default chart;
